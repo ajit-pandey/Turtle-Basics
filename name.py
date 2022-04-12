e5edc658eb0896c46540611fda0327033ad014ba
ajit-pandey <azytpandey19@gmail.com>
2022-04-12 12:05:48 +0545

    information writing in turtke

---
+++ name.py
@@ -0,0 +1,9 @@
+import turtle # importing the module
+trtl = turtle.Turtle() 
+trtl.penup()
+trtl.setpos(-120,100)
+trtl.pendown()
+trtl.pencolor('green')
+trtl.write('Azyt Pandey', font=("Arial", 16, "bold"))
+trtl.penup()
+trtl.ht()
